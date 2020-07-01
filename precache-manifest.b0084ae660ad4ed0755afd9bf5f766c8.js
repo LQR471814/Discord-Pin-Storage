a61340feffa5b9cc3d1be5f4b926b3f0c29d985f
LQR471814 <bramblefern1013@gmail.com>
2020-07-01 13:25:25 -0700

    Updates

---
+++ precache-manifest.b0084ae660ad4ed0755afd9bf5f766c8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ae9d3663a4b4f6ce17bd7076c12017",
+    "url": "/Discord-Pin-Storage/index.html"
+  },
+  {
+    "revision": "fd0f22f1b812336fd96e",
+    "url": "/Discord-Pin-Storage/static/css/main.b46bdfda.chunk.css"
+  },
+  {
+    "revision": "86701ba8d39b296f00df",
+    "url": "/Discord-Pin-Storage/static/js/2.ef750d9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Discord-Pin-Storage/static/js/2.ef750d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0f22f1b812336fd96e",
+    "url": "/Discord-Pin-Storage/static/js/main.96c8fda0.chunk.js"
+  },
+  {
+    "revision": "1cc407ad649a6a162ce6",
+    "url": "/Discord-Pin-Storage/static/js/runtime-main.ac91b7a3.js"
+  },
+  {
+    "revision": "60e0507498bd7eb738f886baa16c3b55",
+    "url": "/Discord-Pin-Storage/static/media/opensans-bold-webfont.60e05074.woff"
+  },
+  {
+    "revision": "cb4b940581f4683b77e0ea292af98ef7",
+    "url": "/Discord-Pin-Storage/static/media/opensans-bold-webfont.cb4b9405.woff2"
+  },
+  {
+    "revision": "03c90227356d30a7d49ad63e98a44c17",
+    "url": "/Discord-Pin-Storage/static/media/opensans-regular-webfont.03c90227.woff"
+  },
+  {
+    "revision": "abba3dcdc168aa1d4dd98de87d8dc320",
+    "url": "/Discord-Pin-Storage/static/media/opensans-regular-webfont.abba3dcd.woff2"
+  },
+  {
+    "revision": "83bd0ada53bddda18ba4bb0d9b15861e",
+    "url": "/Discord-Pin-Storage/static/media/pfp.83bd0ada.png"
+  },
+  {
+    "revision": "c6a40ce3c2b1fa325d9644282bee221d",
+    "url": "/Discord-Pin-Storage/static/media/pfp2.c6a40ce3.png"
+  },
+  {
+    "revision": "5ea083d7238d8b98e3dab95677e79d48",
+    "url": "/Discord-Pin-Storage/static/media/pfp3.5ea083d7.png"
+  },
+  {
+    "revision": "9e9c998e9b78652b00cc9e028f9aa415",
+    "url": "/Discord-Pin-Storage/static/media/robotoslab-variablefont_wght-webfont.9e9c998e.woff"
+  },
+  {
+    "revision": "f86ac424b733e6d2c599a0e5376ddbb2",
+    "url": "/Discord-Pin-Storage/static/media/robotoslab-variablefont_wght-webfont.f86ac424.woff2"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Discord-Pin-Storage/precache-manifest.b0084ae660ad4ed0755afd9bf5f766c8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Discord-Pin-Storage/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
